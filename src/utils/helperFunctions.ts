a377638eee72fa8c45586a6b46cc6fa17e0e8156
לירן <liran12369@gmail.com>
2023-09-06 22:17:41 +0300

    fix helper function only letter initials

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -2,7 +2,7 @@ export const getInitials = (name: string) => {
   const names = name.split(" ");
   let initials = "";
   names.forEach((n) => {
-    initials += n[0];
+    if (n[0]?.match(/[a-z]/i)) initials += n[0];
   });
   return initials;
 };
