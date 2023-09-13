6ad94cb31a1085d5225a095faf29bf2b56126df2
Liran Goldman <liran12369@gmail.com>
2023-09-13 12:37:29 +0300

    add mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,29 +12,33 @@ const Header = () => {
   const session = useSession();
 
   return (
-    <header className="flex items-center bg-background p-5 text-foreground">
-      <span className="mr-5 text-3xl tracking-widest">Harmony Share</span>
-      <ThemeToggle />
-      <nav className="ml-auto flex gap-5 text-lg text-foreground">
-        <Link href="/">Home</Link>
-        <Link href="/playlists">Explore</Link>
-      </nav>
-      <div className="flex items-center">
-        <Dot className="mx-2" />
-        {session.status !== "authenticated" ? (
-          <Button
-            onClick={() =>
-              void signIn("spotify", { callbackUrl: "/dashboard" })
-            }
-          >
-            Sign in
-          </Button>
-        ) : (
-          <div className="flex items-center gap-1">
-            <NotificationDropdown />
-            <AuthAvatar />
-          </div>
-        )}
+    <header className="sticky top-0 flex flex-col items-center gap-5 bg-background p-5 text-foreground md:flex-row  md:gap-0">
+      <div>
+        <span className="mr-5 text-3xl tracking-widest">Harmony Share</span>
+        <ThemeToggle />
+      </div>
+      <div className="flex items-center md:ml-auto ">
+        <nav className=" flex gap-5 text-lg text-foreground">
+          <Link href="/">Home</Link>
+          <Link href="/playlists">Explore</Link>
+        </nav>
+        <div className="flex items-center">
+          <Dot className="mx-2" />
+          {session.status !== "authenticated" ? (
+            <Button
+              onClick={() =>
+                void signIn("spotify", { callbackUrl: "/dashboard" })
+              }
+            >
+              Sign in
+            </Button>
+          ) : (
+            <div className="flex items-center gap-1">
+              <NotificationDropdown />
+              <AuthAvatar />
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
